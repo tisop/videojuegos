a10e51bdc6fc498989d894b384c4578b13b5f027
Simon Oyaneder <simon.oyaneder@gmail.com>
2018-08-20 00:16:11 -0300

    primer comit

---
+++ Assets/cube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+    public float velocidadMovimiento;
+	// Use this for initialization
+	void Start () {
+        velocidadMovimiento = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.Translate(Input.GetAxis("Horizontal") * velocidadMovimiento * Time.deltaTime, 0f, Input.GetAxis("Vertical") * velocidadMovimiento * Time.deltaTime);
+
+    }
+}
